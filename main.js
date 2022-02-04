ac49448cae0d76e831aae341b01d5133b22fe99f
abiton198 <abitonp@gmail.com>
2022-02-04 21:59:41 +0200

    Edit index.html

--- main.js
+++ main.js
@@ -17,12 +17,33 @@ function playerChoice() {
         input = prompt('Type Rock', 'Paper', 'Scissors')
     }
     input = input.toLowerCase();
-    console.log(input)
-
+    let check = validateInput(input);
+    while (check == false) {
+        input = prompt("Type Rock, Paper, Scissors.Spelling needs to be exact, but capitalization doesnt matter");
+    while(input == null){
+        input = prompt('Type Rock', 'Paper', 'Scissors');
+        input = input.toLowerCase();
+    let check = validateInput(input);
+        
+        console.log(input)
+    }
+    
 }
 
 function computerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
+}
+
+function validateInput(choice) {
+    return(choices.includes(choices))
+}
+
+function checkWinner(choiceP, choiceC) {
+    if (choiceP === choiceC) {
+        return 'Tie';
+    } else if((choiceP === "rock" && choiceC == "scissors") || (choiceP === "paper" && choiceC == "rock")) {
+        return 'Player';
+    }
 }
 
 game();
